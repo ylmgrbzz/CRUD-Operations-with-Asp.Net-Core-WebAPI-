3c17487a79e9ed59e3c4fef1179203d4270cc668
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-17 11:09:16 +0300

    CRUD-Operations-with-Asp.Net-Core-WebAPI

---
+++ HotelFinder.DataAccess/Abstract/IHotelRepository.cs
@@ -0,0 +1,18 @@
+﻿using HotelFinderEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess.Abstract
+{
+    public interface IHotelRepository
+    {
+        List<Hotel> GetHotels();
+        Hotel GetHotelById(int id);
+        Hotel CreateHotel(Hotel hotel);
+        Hotel UpdateHotel(Hotel hotel);
+        void DeleteHotel(int id);
+    }
+}
