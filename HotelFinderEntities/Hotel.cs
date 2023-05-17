6b8136870b2dc6932cb42e97ca331124e8d299f0
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-17 10:54:43 +0300

    Proje dosyası ekle.

---
+++ HotelFinderEntities/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelFinderEntities
+{
+    public class Hotel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(50)]
+        public string City { get; set; }
+
+    }
+}
